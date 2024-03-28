7c1000b5d49611539574990f4f35738d1aad79ae
GabrielGosselin <snow.gabo51@gmail.com>
2024-03-28 16:53:21 -0400

    kinda works

--- smooth-scrolling/src/App.jsx
+++ smooth-scrolling/src/App.jsx
@@ -3,6 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Scroll from './Scroll'
+import img from './img/1.png'
+import img2 from './img/2.png'
+import img3 from './img/3.png'
+
 
 function App() {
   
@@ -10,6 +14,9 @@ function App() {
   return (
     <>
     <Scroll/>
+    <img src={img}/>
+    <img src={img2}/>
+    <img src={img3}/>
     <h1>HELOO</h1>
     <h1>HELOO</h1>
     <h1>HELOO</h1>
@@ -26,9 +33,11 @@ function App() {
     <h1>HELOO</h1>
     <h1>HELOO</h1>
     <h1>HELOO</h1>
-    <h1>HELOO</h1>
-    <h1>HELOO</h1>
-    <h1>HELOO</h1>
+   
+   
+    
+
+   
       
     </>
   )
